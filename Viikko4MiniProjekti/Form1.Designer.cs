78cadf0b535cf5c64c335bc609ccde856681269a
jainali <jpainali@yahoo.com>
2018-10-02 10:42:29 +0300

    Liikennepaikat lisätty

--- Viikko4MiniProjekti/Form1.Designer.cs
+++ Viikko4MiniProjekti/Form1.Designer.cs
@@ -39,39 +39,46 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(31, 185);
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(41, 228);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(757, 433);
+            this.listBox1.Size = new System.Drawing.Size(1008, 532);
             this.listBox1.TabIndex = 0;
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(31, 12);
+            this.comboBox1.Location = new System.Drawing.Point(41, 15);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(193, 21);
+            this.comboBox1.Size = new System.Drawing.Size(256, 24);
             this.comboBox1.TabIndex = 1;
             // 
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(349, 12);
+            this.comboBox2.Location = new System.Drawing.Point(465, 15);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(203, 21);
+            this.comboBox2.Size = new System.Drawing.Size(269, 24);
             this.comboBox2.TabIndex = 2;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(31, 52);
+            this.button2.Location = new System.Drawing.Point(41, 64);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(155, 49);
+            this.button2.Size = new System.Drawing.Size(207, 60);
             this.button2.TabIndex = 4;
             this.button2.Text = "Hae";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // monthCalendar1
             // 
-            this.monthCalendar1.Location = new System.Drawing.Point(610, 11);
+            this.monthCalendar1.Location = new System.Drawing.Point(813, 14);
+            this.monthCalendar1.Margin = new System.Windows.Forms.Padding(12, 11, 12, 11);
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.TabIndex = 5;
             // 
@@ -79,23 +86,25 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(31, 144);
+            this.label1.Location = new System.Drawing.Point(41, 177);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 24);
+            this.label1.Size = new System.Drawing.Size(128, 29);
             this.label1.TabIndex = 6;
             this.label1.Text = "Info Label";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(841, 640);
+            this.ClientSize = new System.Drawing.Size(1121, 788);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.monthCalendar1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- Viikko4MiniProjekti/Form1.cs
+++ Viikko4MiniProjekti/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RataDigiTraffic.Model;
+using RataDigiTraffic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,22 @@ namespace Viikko4MiniProjekti
 {
     public partial class Form1 : Form
     {
+        List<Liikennepaikka> lPaikat;
+        List<Juna> junat;
         public Form1()
         {
             InitializeComponent();
+            lPaikat = new List<Liikennepaikka>();
+            junat = new List<Juna>();
+            APIUtil rata = new APIUtil();
+            lPaikat = rata.Liikennepaikat();
+            comboBox1.DataSource = lPaikat.Select(lpaikka => lpaikka.stationName).ToArray();
+            comboBox2.DataSource = lPaikat.Select(lpaikka => lpaikka.stationName).ToArray();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
